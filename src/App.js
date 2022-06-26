b7d391d85dd4a8e5b51d7c984537f7be97e12eaf
Nabiel Alif <nabiel.alif01@gmail.com>
2022-06-26 16:47:17 +0700

    UI update

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <h1 className="app-title">Monsterigo & Monsterina</h1>
+        <h1 className="app-title">Monsters Rolodex</h1>
         <SearchBox
           placeholder="Search monster"
           onChangeHandler={onSearchChange}
