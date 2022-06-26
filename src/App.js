33e3035eda46879e20a12bf560c4be0a8402c108
Nabiel Alif <nabiel.alif01@gmail.com>
2022-06-26 15:40:06 +0700

    Add card component

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ class App extends Component {
     );
     return (
       <div className="App">
+        <h1 className="app-title">Monsterigo & Monsterina</h1>
         <SearchBox
           placeholder="Search monster"
           onChangeHandler={onSearchChange}
--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react";
-import "./card-list.style.css";
+import "./card-list.styles.css";
+import Card from "../card/card-component";
 
 class CardList extends Component {
   render() {
@@ -7,17 +8,7 @@ class CardList extends Component {
     return (
       <div className="card-list">
         {monsters.map(monster => {
-          const { name, id, email } = monster;
-          return (
-            <div className="card-container" key={id}>
-              <img
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-                alt={name}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card monster={monster} />;
         })}
       </div>
     );
